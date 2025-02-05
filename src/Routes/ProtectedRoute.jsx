f4ca207e5de2ae26ae2dcc6eea8eefa6a3fc2a91
Fitzroyfang <Calvinofede@gmail.com>
2025-02-05 11:43:04 -0300

    habilitar login

--- src/Routes/ProtectedRoute.jsx
+++ src/Routes/ProtectedRoute.jsx
@@ -5,9 +5,9 @@ import { Login } from './Login';
 export const ProtectedRoute = ({ children, user, login }) => {
     console.log(user);
 
-    /*if (!user) {
+if (!user) {
         return <Login loginFnct={login} />;
-    }*/
+    }
 
     return children || <Outlet />;
 };
